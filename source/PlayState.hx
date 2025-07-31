5aa6e3558aaefac68fff701aa48aaac64f6db4a7
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-31 09:09:19 -0500

    player hand

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,11 +1,19 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	var playerHand:HandClass;
+
 	override public function create()
 	{
+		playerHand = new HandClass();
+		add(playerHand);
+		playerHand.screenCenter(Y);
+		playerHand.x = FlxG.width - (playerHand.width * 4);
+
 		super.create();
 	}
 
